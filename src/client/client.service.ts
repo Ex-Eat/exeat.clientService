f4feb54c3b59d96b967d4f5726abd98bd82d2e1c
Slash-12 <baptiste.moench@viacesi.fr>
2022-06-30 16:50:05 +0200

    dev: lint et changements mineurs

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,41 +1,43 @@
-import {Connection, Model} from 'mongoose';
+import { Connection, Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
-import {InjectConnection, InjectModel} from '@nestjs/mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Client, ClientDocument } from '../schemas/client.schema';
-import { CreateClientDto, UpdateClientDto } from '../dto/create-client.dto';
-
 
 @Injectable()
 export class ClientService {
-    constructor(
-        @InjectModel(Client.name) private clientModel: Model<ClientDocument>,
-        @InjectConnection() private connection: Connection
-    ) {}
+  constructor(
+    @InjectModel(Client.name) private clientModel: Model<ClientDocument>,
+    @InjectConnection() private connection: Connection,
+  ) {}
 
-    async getAll(): Promise<ClientDocument[]> {
-        return this.clientModel.find().exec();
-    }
+  async getAll(): Promise<ClientDocument[]> {
+    return this.clientModel.find().exec();
+  }
 
-    async getOne(id: number): Promise<ClientDocument> {
-        return await this.clientModel.findOne({globalUserId: id}).exec();
-    }
+  async getOne(id: number): Promise<ClientDocument> {
+    return await this.clientModel.findOne({ globalUserId: id }).exec();
+  }
 
-    async getOneGlobalId(id: number): Promise<ClientDocument> {
-        return await this.clientModel.findOne({ globalUserId: id }).exec();
-    }
+  async getOneGlobalId(id: number): Promise<ClientDocument> {
+    return await this.clientModel.findOne({ globalUserId: id }).exec();
+  }
 
-    async create(createClientDto: CreateClientDto): Promise<ClientDocument> {
-        const createdClient = new this.clientModel(createClientDto);
-        return await createdClient.save();
-    }
+  async create(createClientDto: Client): Promise<ClientDocument> {
+    const createdClient = new this.clientModel(createClientDto);
+    return await createdClient.save();
+  }
 
-    async update(id: number, client: Client): Promise<ClientDocument> {
-        return await this.clientModel.findOneAndUpdate({globalUserId: id}, client, {
-          new: true,
-        });
-    }
+  async update(id: number, client: Client): Promise<ClientDocument> {
+    return await this.clientModel.findOneAndUpdate(
+      { globalUserId: id },
+      client,
+      {
+        new: true,
+      },
+    );
+  }
 
-    async delete(id: number): Promise<ClientDocument> {
-        return await this.clientModel.findOneAndRemove({globalUserId: id});
-    }
+  async delete(id: number): Promise<ClientDocument> {
+    return await this.clientModel.findOneAndRemove({ globalUserId: id });
+  }
 }
